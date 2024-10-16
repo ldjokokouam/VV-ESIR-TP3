cb2b17ad6730a3c9bbe65f9fc102958973646442
ndridi <nourchene.dridi@etudiant.univ-rennes.fr>
2024-10-17 01:16:31 +0200

    Exo 3 Tests

--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -8,4 +8,91 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringUtilsTest {
 
 
+	// Partition 1 : Chaînes équilibrées
+    @Test
+    void testChaineVideBALANCED() {
+        assertTrue(isBalanced(""));
+    }
+
+    @Test
+    void testPaire1SeulSymboleBALANCED() {
+        assertTrue(isBalanced("{}"));
+        assertTrue(isBalanced("[]"));
+        assertTrue(isBalanced("()"));
+    }
+    
+    @Test
+    void testMultiplesPaire1SeulSymboleBALANCED() {
+        assertTrue(isBalanced("{}[]()"));
+        assertTrue(isBalanced("(){}[]"));
+    }
+    
+    @Test
+    void testPairesImbriqueesBALANCED() {
+        assertTrue(isBalanced("{[()]}"));
+        assertTrue(isBalanced("{([])}"));
+        assertTrue(isBalanced("({[]})"));
+        assertTrue(isBalanced("({[]})()"));
+    }
+
+    // Partition 2 : Chaînes déséquilibrées
+    @Test
+    void testPairesUNBALANCED() {
+    	assertFalse(isBalanced("[}"));
+        assertFalse(isBalanced("{]"));
+        assertFalse(isBalanced("[)"));
+    }
+
+
+    @Test
+    void testFermeturesSupplementairesUNBALANCED() {
+        assertFalse(isBalanced("{[]}()}}")); 
+    }
+
+    @Test
+    void testOuverturesSupplementairesUNBALANCED() {
+        assertFalse(isBalanced("{{[()]")); 
+    }
+
+    @Test
+    void testCombinaisonMixteBalancedUNBALANCED() {
+        assertFalse(isBalanced("{[}]({[()]})"));
+    }
+    
+    // Partition 3 : Chaînes contenant uniquement des symboles d'ouverture 
+    @Test
+    void test1SeulSymboleOuvertUNBALANCED() {
+        assertFalse(isBalanced("{"));
+        assertFalse(isBalanced("["));
+        assertFalse(isBalanced("("));
+    }
+    
+    @Test
+    void testPlusieursSymbolesOuvertsUNBALANCED() {
+        assertFalse(isBalanced("{{[(")); 
+    }
+
+    // Partition 4 : Chaînes contenant uniquement des symboles de fermeture
+    @Test
+    void test1SeulSymboleFermeUNBALANCED() {
+        assertFalse(isBalanced("}"));
+        assertFalse(isBalanced("]"));
+        assertFalse(isBalanced(")"));
+    }
+    
+    @Test
+    void testPlusieursSymbolesFermesUNBALANCED() {
+        assertFalse(isBalanced("}}}")); 
+    }
+    
+    
+
 }
+    
+    
+    
+    
+
+
+
+
